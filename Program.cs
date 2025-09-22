2cc1a6413c5cacf6899d5a60f549913a24a3fb37
italolimma <italoaslima@live.com>
2025-09-22 14:27:47 -0300

    implementado as áreas em falta no desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "12345678", modelo: "Modelo 1", imei: "10101101", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "98765432", modelo: "Modelo 2", imei: "01010010", memoria: 256);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
